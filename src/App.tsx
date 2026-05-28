35eec04cdbf157c2c1abbeec3d4a6dcc7a07dc95
Alisson Freitas <alissonfreitas031@gmail.com>
2026-05-28 14:03:42 -0300

    feat: sorteando a palavra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import styles from "./app.module.css"
 
+import { useEffect, useState } from "react"
+import { WORDS, type Challenge } from "./utils/words"
+
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
@@ -8,15 +11,32 @@ import { Button } from "./components/Button"
 import { LettersUsed } from "./components/LettersUsed"
 
 export default function App() {
+  const [attempts, setAttempts] = useState(0)
+  const [letter, setLetter] = useState("")
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
 
   function handleRestartGame() {
     alert("Reiniciar o jogo!")
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect( () => {
+    startGame()
+  }, [])
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagem de programação mais utilizadas" />
 
